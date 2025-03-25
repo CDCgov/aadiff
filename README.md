df6cab18ed81713fd20963f209930dbac09d3af5
Samuel Shepard <vfn4@cdc.gov>
2025-03-25 14:03:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# AA Diff
+
+A port of work by James Smagala for acceleration of RainbowTree and other use cases.
+
+## Legacy algorithm
+
+### Requirements
+
+- First sequence is used as the reference
+- Must be DNA
+- Must be aligned
+
+### Disambiguation
+
+- Must be IUPAC
+- Reference is not disambiguated
+- All possible amino acid translation will be reported.
---
+++ src/main.rs
@@ -0,0 +1,116 @@
+#![feature(let_chains)]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
+
+use clap::Parser;
+use either::Either;
+use std::{
+    fs::OpenOptions,
+    io::{BufReader, BufWriter, Write, stdin, stdout},
+    path::PathBuf,
+};
+use zoe::prelude::*;
+
+// TODO: we want to change the delimiter
+
+/// Holds arguments for the APD CLI.
+#[derive(Debug, Parser)]
+#[command(about = "Tool for calculating average-pairwise distances")]
+pub struct APDArgs {
+    #[arg(short = 'i', long)]
+    /// Optional input fasta
+    input_fasta: Option<PathBuf>,
+
+    #[arg(short = 'o', long)]
+    /// Optional output delimited
+    output_tsv: Option<PathBuf>,
+
+    #[arg(short = 't', long)]
+    /// Turns on simultaneous multi-threading
+    multi_threaded: bool,
+}
+
+/// CLI entry point
+fn main() {
+    let args = APDArgs::parse();
+
+    let mut reader = if let Some(ref file_path) = args.input_fasta {
+        FastaReader::new(BufReader::new(Either::Left(
+            OpenOptions::new()
+                .read(true)
+                .open(file_path)
+                .expect("File opening error"),
+        )))
+    } else {
+        FastaReader::new(BufReader::new(Either::Right(stdin())))
+    };
+
+    let mut writer = if let Some(ref file_path) = args.output_tsv {
+        BufWriter::new(Either::Left(
+            OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(file_path)
+                .expect("File write error"),
+        ))
+    } else {
+        BufWriter::new(Either::Right(stdout()))
+    };
+
+    let Some(Ok(dna_reference)) = reader.next() else {
+        eprintln!("No first record available!");
+        std::process::exit(1);
+    };
+    // TODO: recode to IUPAC if not in IUPAC.
+    let reference = dna_reference.translate();
+
+    let other_sequences = reader
+        .map(|record| 
+            // TODO: recode to IUPAC
+            // TODO: don't translate, instead defer until later
+            record.map(|r| r.translate()) 
+              
+        )
+        .collect::<Result<Vec<_>,_>>().unwrap_or_die("Could not process other data.");
+
+
+    let mut buffer = format!(",{}", reference.name);
+    for query_header in other_sequences.iter().map(|f| f.name.as_str()) {
+        buffer.push(',');
+        buffer.push_str(query_header);
+    }
+    writeln!(&mut writer, "{buffer}").unwrap_or_fail();
+
+    for (i, ref_aa) in reference.sequence.into_iter().enumerate() {
+        let mut differences_found = false;
+        buffer.clear();
+
+        // TODO: grab codon in the future instead and consider not resolvable ambiguations
+        // TODO: check all sequences have same length earlier
+        // TODO: make sure you use uppercase for both
+        // TODO: do we annotate deletions?
+        for query_aa in other_sequences.iter().map(|f| f.sequence[i]) {
+            if ref_aa != query_aa && query_aa.is_ascii_alphabetic() && ref_aa.is_ascii_alphabetic()
+            {
+                buffer.push_str(r#",""#);
+                buffer.push(query_aa as char);
+                buffer.push('"');
+                differences_found = true;
+            } else {
+                buffer.push(',');
+            }
+        }
+
+        if differences_found {
+            writeln!(
+                &mut writer,
+                "{p},{aa}{buffer}",
+                p = i + 1,
+                aa = ref_aa as char
+            )
+            .unwrap_or_fail();
+        }
+    }
+
+    writer.flush().unwrap_or_fail();
+}
